cec24c431fc7b496f5fce5c85f10c9c88d912a16
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2023-02-02 16:02:52 +0900

    오더화면구현

---
+++ CloneStarbucks/Page/OrderPage.swift
@@ -0,0 +1,21 @@
+//
+//  OrderPage.swift
+//  CloneStarbucks
+//
+//  Created by 최지철 on 2023/02/02.
+//
+
+import UIKit
+
+class OrderPage: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
